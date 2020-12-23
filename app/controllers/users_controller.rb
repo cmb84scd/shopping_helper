074e4c7158d52f41a3b2c61e68f1ed531273f623
cmb84scd <cmb84scd@gmail.com>
2020-12-23 16:58:17 +0000

    Add create account to navbar and fix rubocop issues

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -56,13 +56,13 @@ class UsersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @user = User.find(params[:id])
+  end
 
-    # Only allow a list of trusted parameters through.
-    def user_params
-      params.require(:user).permit(:username, :email, :password, :password_confirmation)
-    end
+  # Only allow a list of trusted parameters through.
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+  end
 end
--- spec/controllers/users_spec.rb
+++ spec/controllers/users_spec.rb
@@ -1,4 +1,4 @@
- require 'rails_helper'
+require 'rails_helper'
 
 RSpec.describe "/users", type: :request do
   let(:valid_attributes) { { username: 'testuser', email: 'test@email.com', password: 'password' } }
