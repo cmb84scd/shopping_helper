dcc6896bc8c1d396c75a6f374754ac8dc542c437
cmb84scd <cmb84scd@gmail.com>
2020-12-23 11:24:38 +0000

    Add generate users files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_17_152057) do
+ActiveRecord::Schema.define(version: 2020_12_23_111337) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 2020_12_17_152057) do
     t.string "item"
     t.integer "aisle"
     t.string "side"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
