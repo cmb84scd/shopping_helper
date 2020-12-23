f922a2d5b8631e60f8b3b222f1daa6281db54b98
cmb84scd <cmb84scd@gmail.com>
2020-12-23 16:26:58 +0000

    Add update user profile feature tests

---
+++ spec/features/delete_user_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.feature 'Delete User', type: :feature do
+  scenario 'User can delete their account' do
+    user1 = User.create(username: 'testuser', email: 'test@email.com', password: 'password')
+
+    visit "users/#{user1.id}"
+
+    expect(page).to have_content 'User Profile'
+
+    click_link 'Delete Account'
+
+    expect(page).to_not have_content('testuser')
+    expect(page).to_not have_content('test@email.com')
+    expect(page).to have_content('Account has successfully been deleted.')
+  end
+end
