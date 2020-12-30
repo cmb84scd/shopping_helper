e36373d4f631a9e6f42a8eb5afd4e73a9a99ee48
cmb84scd <cmb84scd@gmail.com>
2020-12-30 11:03:43 +0000

    Add generate sessions controller files

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  resources :users, except: :index
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+  
   root     'products#index'
+  resources :users, except: :index
   resources :products
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
