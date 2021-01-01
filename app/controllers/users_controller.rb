7466fe13418910da7848fed21a71823689d491ef
cmb84scd <cmb84scd@gmail.com>
2021-01-01 16:42:58 +0000

    Add require login method to application controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  skip_before_action :require_login, only: [:new, :create]
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   # GET /users/1
@@ -22,7 +23,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to root_url, notice: 'Account was successfully created.' }
+        format.html { redirect_to root_url, notice: 'Account successfully created. Please log in!' }
         format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new }
@@ -50,7 +51,7 @@ class UsersController < ApplicationController
   def destroy
     @user.destroy
     respond_to do |format|
-      format.html { redirect_to root_url, notice: 'Account has successfully been deleted.' }
+      format.html { redirect_to root_url, alert: 'Account has successfully been deleted.' }
       format.json { head :no_content }
     end
   end
