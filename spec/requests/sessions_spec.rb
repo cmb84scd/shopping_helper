b410810dfaa348368cbe372c6ddbf715bbc5a9b5
cmb84scd <cmb84scd@gmail.com>
2021-01-06 18:26:52 +0000

    Add invalid login details sessions tests

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe "Sessions", type: :request do
 
+  User.create(username: 'testuser', email: 'test@email.com', password: 'password')
+
   describe "GET /new" do
     it "returns http success" do
       get "/sessions/new"
@@ -10,15 +12,39 @@ RSpec.describe "Sessions", type: :request do
   end
 
   describe "POST /create" do
-    it "returns http success" do
-      User.create(username: 'testuser', email: 'test@email.com', password: 'password')
-      post "/sessions", params: { 'user[email]' => 'test@email.com', 'user[password]' => 'password' }
-      expect(response).to have_http_status(302)
+    context "with valid login details" do
+      it "returns http 302" do
+        post "/sessions", params: { 'user[email]' => 'test@email.com', 'user[password]' => 'password' }
+        expect(response).to have_http_status(302)
+      end
+
+      it 'redirects to the products page' do
+        post "/sessions", params: { 'user[email]' => 'test@email.com', 'user[password]' => 'password' }
+        expect(response).to redirect_to(products_url)
+      end
+    end
+
+    context "with invalid login details" do
+      it "returns http 302 with invalid password" do
+        post "/sessions", params: { 'user[email]' => 'test@email.com', 'user[password]' => 'pa5sw0rd' }
+        expect(response).to have_http_status(302)
+      end
+
+      it "returns http 302 with invalid email" do
+        post "/sessions", params: { 'user[email]' => 'user@email.com', 'user[password]' => 'password' }
+        expect(response).to have_http_status(302)
+      end
+
+      it 'redirects to the login page' do
+        post "/sessions", params: { 'user[email]' => 'test@email.com', 'user[password]' => 'pa5sw0rd' }
+        expect(response).to redirect_to(sessions_new_url)
+      end
     end
   end
 
   describe "GET /destroy" do
     it "returns http 302" do
+      post "/sessions", params: { 'user[email]' => 'test@email.com', 'user[password]' => 'password' }
       get "/sessions/destroy"
       expect(response).to have_http_status(302)
     end
