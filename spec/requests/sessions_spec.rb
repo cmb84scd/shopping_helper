1d7523d6ba2c51960b92fcc0e8c42bd0f69e88eb
cmb84scd <cmb84scd@gmail.com>
2021-01-07 16:23:15 +0000

    Amend requests sessions spec so that data clears from DB

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -1,8 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "Sessions", type: :request do
-
-  User.create(username: 'testuser', email: 'test1@email.com', password: 'password')
+  let(:user1) { User.create(username: 'testuser', email: 'test@email.com', password: 'password') }
 
   describe "GET /new" do
     it "returns http success" do
@@ -14,29 +13,29 @@ RSpec.describe "Sessions", type: :request do
   describe "POST /create" do
     context "with valid login details" do
       it "returns http 302" do
-        post "/sessions", params: { 'user[email]' => 'test1@email.com', 'user[password]' => 'password' }
+        post "/sessions", params: { 'user[email]' => user1.email, 'user[password]' => user1.password }
         expect(response).to have_http_status(302)
       end
 
       it 'redirects to the products page' do
-        post "/sessions", params: { 'user[email]' => 'test1@email.com', 'user[password]' => 'password' }
+        post "/sessions", params: { 'user[email]' => user1.email, 'user[password]' => user1.password }
         expect(response).to redirect_to(products_url)
       end
     end
 
     context "with invalid login details" do
       it "returns http 302 with invalid password" do
-        post "/sessions", params: { 'user[email]' => 'test1@email.com', 'user[password]' => 'pa5sw0rd' }
+        post "/sessions", params: { 'user[email]' => user1.email, 'user[password]' => 'pa5sw0rd' }
         expect(response).to have_http_status(302)
       end
 
       it "returns http 302 with invalid email" do
-        post "/sessions", params: { 'user[email]' => 'user@email.com', 'user[password]' => 'password' }
+        post "/sessions", params: { 'user[email]' => 'user@email.com', 'user[password]' => user1.password }
         expect(response).to have_http_status(302)
       end
 
       it 'redirects to the login page' do
-        post "/sessions", params: { 'user[email]' => 'test1@email.com', 'user[password]' => 'pa5sw0rd' }
+        post "/sessions", params: { 'user[email]' => user1.email, 'user[password]' => 'pa5sw0rd' }
         expect(response).to redirect_to(sessions_new_url)
       end
     end
@@ -44,9 +43,15 @@ RSpec.describe "Sessions", type: :request do
 
   describe "GET /destroy" do
     it "returns http 302" do
-      post "/sessions", params: { 'user[email]' => 'test1@email.com', 'user[password]' => 'password' }
+      post "/sessions", params: { 'user[email]' => user1.email, 'user[password]' => user1.password }
       get "/sessions/destroy"
       expect(response).to have_http_status(302)
+    end
+
+    it "redirects to the home page" do
+      post "/sessions", params: { 'user[email]' => user1.email, 'user[password]' => user1.password }
+      get "/sessions/destroy"
+      expect(response).to redirect_to(root_url)
     end
   end
 end
