164d786862397a3a6ee3c306fc35bb65a3ea55a8
cmb84scd <cmb84scd@gmail.com>
2021-01-08 10:58:56 +0000

    Refactor must be logged in feature test

--- spec/features/must_be_logged_in_spec.rb
+++ spec/features/must_be_logged_in_spec.rb
@@ -3,11 +3,13 @@ require 'rails_helper'
 RSpec.feature 'Must Be Logged In', type: :feature do
   scenario 'User unable to visit product page when not logged in' do
     visit '/products'
+    expect(current_path).to eq root_path
     expect(page).to have_content('You must be logged in to access this section!')
   end
 
   scenario 'User unable to visit profile page when not logged in' do
     visit '/users/1'
+    expect(current_path).to eq root_path
     expect(page).to have_content('You must be logged in to access this section!')
   end
 end
