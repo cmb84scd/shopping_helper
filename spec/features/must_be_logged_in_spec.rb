1662a026df2823f3cfa907e409f6db561d76a53b
cmb84scd <cmb84scd@gmail.com>
2021-01-14 17:16:44 +0000

    Change URL in must_be_logged_in_spec

--- spec/features/must_be_logged_in_spec.rb
+++ spec/features/must_be_logged_in_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.feature 'Must Be Logged In', type: :feature do
   scenario 'User unable to visit product page when not logged in' do
-    visit '/products'
+    visit products_url
     expect(current_path).to eq root_path
     expect(page).to have_content('You must be logged in to access this section!')
   end
