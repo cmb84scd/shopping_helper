9793ea4e089ca7bad67f9df81c7be00c5dd8114b
cmb84scd <cmb84scd@gmail.com>
2021-01-18 16:33:25 +0000

    Fix/add model test

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user1) { User.create(username: 'testuser', email: 'test@email.com', password: 'password') }
+  let(:product1) { Product.create(item: 'bread', aisle: 5, side: 'Left', user_id: user1.id) }
+  
+  it 'User item input to be valid' do
+    item = Item.create(user_id: user1.id, product_id: product1.id, quantity: 1)
+    expect(item).to be_valid
+  end
 end
