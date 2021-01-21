2fb91afc0d1a0fbd7579472fb6bb6a7ac6dc97f6
cmb84scd <cmb84scd@gmail.com>
2021-01-21 14:50:55 +0000

    Add feature test for deleting item from shopping list

---
+++ spec/features/delete_item_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.feature 'Delete Item', type: :feature do
+  scenario 'Can remove item from shopping list only' do
+    login_user
+    click_link 'ShoppingList'
+    click_link 'Delete'
+
+    expect(page).to have_content('Item was successfully deleted.')
+    expect(page).to_not have_content('bread')
+    expect(page).to_not have_content(5)
+    expect(page).to_not have_content('Left')
+
+    click_link 'ShoppingHelper'
+
+    expect(page).to have_content('bread')
+  end
+end
