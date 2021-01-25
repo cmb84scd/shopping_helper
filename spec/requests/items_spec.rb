592eb728d1c7e6633dca22c4cd3f2610b7998eae
cmb84scd <cmb84scd@gmail.com>
2021-01-25 15:17:21 +0000

    Change quote marks from single to double in request spec

--- spec/requests/items_spec.rb
+++ spec/requests/items_spec.rb
@@ -1,51 +1,51 @@
 require 'rails_helper'
 
-RSpec.describe "/items", type: :request do
+RSpec.describe '/items', type: :request do
   let(:user1) { User.create(username: 'testuser', email: 'test@email.com', password: 'password') }
   let(:product1) { Product.create(item_name: 'bread', aisle: 5, side: 'Left', user_id: user1.id) }
   let(:valid_attributes) { { user_id: user1.id, product_id: product1.id, quantity: 1 } }
 
   setup { sign_in_as user1 }
 
-  describe "GET /index" do
-    it "renders a successful response" do
+  describe 'GET /index' do
+    it 'renders a successful response' do
       Item.create! valid_attributes
       get items_url
       expect(response).to be_successful
     end
   end
 
-  describe "GET /edit" do
-    it "render a successful response" do
+  describe 'GET /edit' do
+    it 'render a successful response' do
       item = Item.create! valid_attributes
       get edit_item_url(item)
       expect(response).to be_successful
     end
   end
 
-  describe "POST /create" do
-    context "with valid parameters" do
-      it "creates a new Item" do
+  describe 'POST /create' do
+    context 'with valid parameters' do
+      it 'creates a new Item' do
         expect {
           post items_url, params: { product_id: product1.id }
         }.to change(Item, :count).by(1)
       end
 
-      it "redirects to the created item" do
+      it 'redirects to the created item' do
         post items_url, params: { product_id: product1.id }
         expect(response).to redirect_to(products_url)
       end
     end
 
-    context "with invalid parameters" do
-      it "does not create a new Item" do
+    context 'with invalid parameters' do
+      it 'does not create a new Item' do
         Item.create! valid_attributes
         expect {
           post items_url, params: { product_id: product1.id }
         }.to change(Item, :count).by(0)
       end
 
-      it "renders a redirect response (i.e. to display the products template)" do
+      it 'renders a redirect response (i.e. to display the products template)' do
         Item.create! valid_attributes
         post items_url, params: { product_id: product1.id }
         expect(response).to redirect_to(products_url)
@@ -53,18 +53,18 @@ RSpec.describe "/items", type: :request do
     end
   end
 
-  describe "PATCH /update" do
-    context "with valid parameters" do
+  describe 'PATCH /update' do
+    context 'with valid parameters' do
       let(:new_attributes) { { user_id: user1.id, product_id: product1.id, quantity: 2 } }
 
-      it "updates the requested item" do
+      it 'updates the requested item' do
         item = Item.create! valid_attributes
         patch item_url(item), params: { item: new_attributes }
         item.reload
         expect(Item.find_by(quantity: 2)).to be
       end
 
-      it "redirects to the item" do
+      it 'redirects to the item' do
         item = Item.create! valid_attributes
         patch item_url(item), params: { item: new_attributes }
         item.reload
@@ -73,15 +73,15 @@ RSpec.describe "/items", type: :request do
     end
   end
 
-  describe "DELETE /destroy" do
-    it "destroys the requested item" do
+  describe 'DELETE /destroy' do
+    it 'destroys the requested item' do
       item = Item.create! valid_attributes
       expect {
         delete item_url(item)
       }.to change(Item, :count).by(-1)
     end
 
-    it "redirects to the items list" do
+    it 'redirects to the items list' do
       item = Item.create! valid_attributes
       delete item_url(item)
       expect(response).to redirect_to(items_url)
