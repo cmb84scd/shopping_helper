a5bfdcacb692c9adb5b9c27808b2e6b31a034520
cmb84scd <cmb84scd@gmail.com>
2021-01-25 17:32:02 +0000

    Add unique index to product_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_18_172856) do
+ActiveRecord::Schema.define(version: 2021_01_25_171819) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2021_01_18_172856) do
     t.integer "quantity"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["product_id"], name: "index_items_on_product_id"
+    t.index ["product_id"], name: "index_items_on_product_id", unique: true
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
